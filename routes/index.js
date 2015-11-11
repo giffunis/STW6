50d87546f7e6244081e62e4745fd53b241acd221
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-11-11 15:00:54 +0000

    index,js and quiz_contreller.js modified

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
 /* GET quizes/question page. */
-router.get('/quizes/question', function(req, res, next){
-  res.render('quizes/question.ejs');
-});
+router.get('/quizes/question', quizController.question);
 
 /* GET quizes/answer page.*/
-router.get('/quizes/answer', function(req, res, next){
-  res.render('quizes/answer.ejs');
-});
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
