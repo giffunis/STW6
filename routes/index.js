b7145822e8eba8a0ea69369adcb464382f758672
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-11-11 14:45:26 +0000

    added get routes to index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET quizes/question page. */
+router.get('/quizes/question', function(req, res, next){
+  res.render('quizes/question.ejs');
+});
+
+/* GET quizes/answer page.*/
+router.get('/quizes/answer', function(req, res, next){
+  res.render('quizes/answer.ejs');
+});
+
 module.exports = router;
